0c6c3772bcdc0a8cca346426ed46f7777bb68068
nghiahv <nghiahv@d-soft.com.vn>
2024-06-26 19:25:01 -0700

    feat:

--- lib/presentation/dashboard/dashboard_screen.dart
+++ lib/presentation/dashboard/dashboard_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:footballmanager/domain/serviceable/auth_serviceable.dart';
 import 'package:footballmanager/presentation/dashboard/dashboard_controller.dart';
 import 'package:footballmanager/presentation/field/field_screen.dart';
 import 'package:footballmanager/presentation/match/match_all_screen.dart';
@@ -26,8 +27,7 @@ class MyDashBoard extends StatefulWidget {
 class _MyDashBoardState extends State<MyDashBoard> {
   MyDashBoardController controller = MyDashBoardController.to;
   MatchController matchController = MatchController.to;
-
-
+  final authStore = AuthStore.to;
   @override
   Widget build(BuildContext context) {
     return GetBuilder<MyDashBoardController>(
@@ -61,9 +61,16 @@ class _MyDashBoardState extends State<MyDashBoard> {
               unselectedLabelStyle: AppTextStyles.regular11,
               onTap: (index) async {
                 if(index == 2)
-                  {  await matchController.getMatchesByTeam();
-                      matchController.getMatchesAll();
-                    Get.to(MatchAllScreen(), routeName: Routers.matchall);
+
+                  {authStore.isLogged.value == true ?
+                    {
+                  await matchController.getMatchesByTeam(),
+                    matchController.getMatchesAll(),
+                  Get.to(MatchAllScreen(), routeName: Routers.matchall),
+                    } : {
+                    await matchController.getMatchesAll(),
+                    Get.to(MatchAllScreen(), routeName: Routers.matchall),
+                  };
                   }
                 else
                   {
